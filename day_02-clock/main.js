cffdbaebafe1b85ce3691901b5c845c85262610d
Mohamed Hassan <mr10dev10@gmail.com>
2021-06-12 01:13:22 +0200

    2

--- day_02-clock/main.js
+++ day_02-clock/main.js
@@ -1,6 +1,43 @@
-let dateObj = new Date(),
+/*let secsWise = document.getElementById('secs-wise'),
+	minsWise = document.getElementById('mins-wise'),
+	hoursWise = document.getElementById('hours-wise');*/
+
+	/*let dateObj2 = new Date(),
+	secs2 = dateObj2.getUTCSeconds(),
+	mins2 = dateObj2.getUTCMinutes(),
+	hours2 = ((dateObj2.getUTCHours() + 2) % 12);
+
+	secsWise.style.cssText = `transform: translate(-50%, 0) rotate(${6 * secs2}deg)`;
+	minsWise.style.cssText = `transform: translate(-50%, 0) rotate(${6 * mins2}deg)`;
+	hoursWise.style.cssText = `transform: translate(-50%, 0) rotate(${30 * hours2}deg)`;
+
+setInterval(() => {
+	let dateObj = new Date(),
 	secs = dateObj.getUTCSeconds(),
 	mins = dateObj.getUTCMinutes(),
 	hours = ((dateObj.getUTCHours() + 2) % 12);
+	if(secs == 0) {
+		secsWise.classList.add('no-transition');
+	} else {
+		secsWise.classList.remove('no-transition');
+	}
 
-	console.log(hours);
+	if(mins == 0) {
+		minsWise.classList.add('no-transition');
+	} else {
+		minsWise.classList.remove('no-transition');
+	}
+
+	if(hours == 0) {
+		hoursWise.classList.add('no-transition');
+	} else {
+		hoursWise.classList.remove('no-transition');
+	}
+
+	secsWise.style.cssText = `transform: translate(-50%, 0) rotate(${6 * secs}deg)`;
+	minsWise.style.cssText = `transform: translate(-50%, 0) rotate(${6 * mins}deg)`;
+	hoursWise.style.cssText = `transform: translate(-50%, 0) rotate(${30 * hours}deg)`;
+}, 1000);*/
+
+
+
