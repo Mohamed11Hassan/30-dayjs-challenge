ec59abb8efc860b26cd12bb01be4ddaccc865905
Mohamed Hassan <mr10dev10@gmail.com>
2021-06-14 00:27:19 +0200

    2

--- day_02-clock/main.js
+++ day_02-clock/main.js
@@ -11,14 +11,18 @@ for(let i = 0;i <= 59;i++){
 	clockNumbers[i].style.cssText = `transform: translate(-50%, 0) rotate(${i * 6}deg)`;
 }
 
-let secHand = document.getElementById('secHand');
+let secHand = document.getElementById('secHand'),
+ 	minHand = document.getElementById('minHand'),
+ 	hourHand = document.getElementById('hourHand');
 
 	let dateObj2 = new Date(),
 	secs2 = dateObj2.getUTCSeconds(),
 	mins2 = dateObj2.getUTCMinutes(),
 	hours2 = ((dateObj2.getUTCHours() + 2) % 12);
 
-	secHand.style.cssText = `transform: translate(-50%, 0) rotate(${(6 * secs2) + 180}deg)`;
+	secHand.style.cssText = `transform: translate(0%, -50%) rotate(${((secs2 / 60) * 360) + 90}deg)`;
+	minHand.style.cssText = `transform: translate(0%, -50%) rotate(${((mins2 / 60) * 360) + 90}deg)`;
+	hourHand.style.cssText = `transform: translate(0%, -50%) rotate(${((hours2 / 60) * 360) + 90}deg)`;
 
 setInterval(() => {
 	let dateObj = new Date(),
@@ -26,11 +30,38 @@ setInterval(() => {
 	mins = dateObj.getUTCMinutes(),
 	hours = ((dateObj.getUTCHours() + 2) % 12);
 
+
+	// handling the seconds clock hand rotation 
 	if(secs == 0) {
-		secHand.style.cssText='transition: none';
+
+		secHand.style.transition = 'all 0.6s cubic-bezier(0.9, 0, 0, 1) 0s';
+		secHand.style.transform = `translate(0%, -50%) rotate(${450}deg)`;
+
+		setTimeout(function() {
+			secHand.style.transition = 'none';
+			secHand.style.transform = `translate(0%, -50%) rotate(${90}deg)`;
+		}, 610);
+
+	} else if(secs != 0){
+		secHand.style.transform = `translate(0%, -50%) rotate(${((secs / 60) * 360) + 90}deg)`;
+		secHand.style.transition = 'all 0.6s cubic-bezier(0.9, 0, 0, 1) 0s';
 	}
 
-	secHand.style.cssText = `transform: translate(-50%, 0) rotate(${(6 * secs) + 180}deg);transition: all 0.6s cubic-bezier(0.9, 0, 0, 1) 0s`;
+	// handling the mins clock hand rotation 
+	if(mins == 0) {
+
+		minHand.style.transition = 'all 0.6s cubic-bezier(0.9, 0, 0, 1) 0s';
+		minHand.style.transform = `translate(0%, -50%) rotate(${450}deg)`;
+
+		setTimeout(function() {
+			minHand.style.transition = 'none';
+			minHand.style.transform = `translate(0%, -50%) rotate(${90}deg)`;
+		}, 610);
+
+	} else if(mins != 0){
+		minHand.style.transform = `translate(0%, -50%) rotate(${((mins / 60) * 360) + 90}deg)`;
+		minHand.style.transition = 'all 0.6s cubic-bezier(0.9, 0, 0, 1) 0s';
+	}
 
 }, 1000);
 
